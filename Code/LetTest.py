330c46e76227e0b7d02fee7419951f6b1068b786
Seraphin Sampers Account <74018386+SampersS@users.noreply.github.com>
2023-10-02 14:53:23 +0200

    Update LetTest.py

--- Code/LetTest.py
+++ Code/LetTest.py
@@ -37,50 +37,18 @@ while True:
 
     strip.setPixelColor(0, Color(255,255,255, 255))
 
-    strip.setPixelColor(1, Color(0,0,0))
+    strip.setPixelColor(1, Color(0,0,0,0))
 
     strip.show()
 
+    time.sleep(5)
+
+    strip.setPixelColor(0, Color(44,44,44,0))
+
+    strip.setPixelColor(1, Color(0,0,0,255))
+
+    strip.show()
     
+    time.sleep(2)
 
-    time.sleep(0.5)
 
-    # Second LED white
-
-    strip.setPixelColor(0, Color(0,0,0))
-
-    strip.setPixelColor(1, Color(255,255,255, 255))
-
-    strip.show()
-
-    time.sleep(1)
-
-    # LEDs Green
-
-    strip.setPixelColor(0, Color(255,255,255, 255))
-
-    strip.setPixelColor(1, Color(255,0,0))
-
-    strip.show()
-
-    time.sleep(0.5)
-
-    # LEDs Red
-
-    strip.setPixelColor(0, Color(0,255,0))
-
-    strip.setPixelColor(1, Color(255,255,255, 255))
-
-    strip.show()
-
-    time.sleep(0.5)
-
-    # LEDs Blue
-
-    strip.setPixelColor(0, Color(255,255,255, 255))
-
-    strip.setPixelColor(1, Color(0,0,255))
-
-    strip.show()
-
-    time.sleep(1)
