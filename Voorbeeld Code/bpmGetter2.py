1bab5f3b9d699a3f6b4728bec1526291bfb6e616
Seraphin Sampers Account <74018386+SampersS@users.noreply.github.com>
2023-10-09 14:41:02 +0200

    mqtt subscribe voorbeeldcode upload
    we kunnen dit hopelijk ook gebruiken op de pi

--- Voorbeeld Code/bpmGetter2.py
+++ Voorbeeld Code/bpmGetter2.py
@@ -1,30 +1,24 @@
-import librosa
-import RPi.GPIO as GPIO
-import time
+import sounddevice as sd
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.io import wavfile
 
-# Configureer GPIO-pinnen voor LEDs
-GPIO.setmode(GPIO.BCM)
-led_pin = 18
-GPIO.setup(led_pin, GPIO.OUT)
+fs = 44100  
+duration = 5
 
-# Functie om LEDs te laten knipperen met een bepaalde snelheid (BPM)
-def blink_LED(bpm):
-    interval = 60.0 / bpm  # Bereken de tijd tussen knippers op basis van BPM
-    while True:
-        GPIO.output(led_pin, GPIO.HIGH)
-        time.sleep(interval / 2)
-        GPIO.output(led_pin, GPIO.LOW)
-        time.sleep(interval / 2)
+def getTotal(data):
+    total = 0
+    for i in range(len(data)):
+        total += data[i]
+    return total
 
-# Functie om BPM van mp4-bestand te detecteren
-def detect_BPM(mp4_file):
-    y, sr = librosa.load(mp4_file)
-    onset_env = librosa.onset.onset_strength(y=y, sr=sr)
-    tempo, _ = librosa.beat.beat_track(onset_envelope=onset_env, sr=sr)
-    return tempo
+def callback(indata, outdata, frames, time, status):
+    if status:
+        print(status)
+    outdata[:] = indata
 
-if __name__ == "__main__":
-    mp4_file = "jouw_bestand.mp4"  # Vervang dit door het pad naar jouw mp4-bestand
-    bpm = detect_BPM(mp4_file)
-    print(f"Gedetecteerde BPM: {bpm}")
-    blink_LED(bpm)
+with sd.Stream(channels=2, callback=callback):
+    sd.sleep(int(duration * 1000))
+
+print("Opnemen... druk ctrl+c om te stoppen")
+audio_data = sd.Stream()
