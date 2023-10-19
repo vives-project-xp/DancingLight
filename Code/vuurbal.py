ee3c1fff6ac96103265762032fd710f79d0007df
Seraphin Sampers Account <74018386+SampersS@users.noreply.github.com>
2023-10-19 18:11:48 +0200

    upload van lokale bestanden + cleanup
    kajefnakfjnakfjnzaof

---
+++ Code/vuurbal.py
@@ -0,0 +1,20 @@
+#nog niet klaar
+import time
+import random
+
+# Configure the Neopixel ring
+Levels = [(50,0,0),(100,0,0),(150,0,0),(200,0,0),(255,0,0),(200,0,0),(150,0,0),(100,0,0),(50,0,0)]
+
+rows = 12
+colums = 10
+ywaarde=0
+xwaarde=6
+#random.seed(69)
+while True:
+    pixels[xwaarde, ywaarde] = Levels[0]
+
+    for i in range(len(Levels)):
+        
+
+    time.sleep(0.5)
+
