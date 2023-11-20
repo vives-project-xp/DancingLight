3e5e362e35f80f5c809f813e023d7d2a3947a185
William Rogov <flexerwithepether@gmail.com>
2023-11-20 09:55:00 +0100

    minor adjustment

--- Code/ConnectStrips.py
+++ Code/ConnectStrips.py
@@ -2,22 +2,22 @@ import board
 import neopixel
 import time
 
-# Definieer de configuratie van de ledstrips
-led_pin_1 = board.D18  # Pin voor de eerste ledstrip
-led_pin_2 = board.D12  # Pin voor de tweede ledstrip
-num_leds_per_strip = 60  # Aantal leds per ledstrip
+# Define and configurate LED's strips
+led_pin_1 = board.D18  # Pin first Led-strip
+led_pin_2 = board.D12  # Pin second Led-strip
+num_leds_per_strip = 60  # Amount of LED's in 1 strip
 
-# Maak de Neopixel-objecten aan voor elke ledstrip
+#Turn every LED on
 strip1 = neopixel.NeoPixel(led_pin_1, num_leds_per_strip, auto_write=False)
 strip2 = neopixel.NeoPixel(led_pin_2, num_leds_per_strip, auto_write=False)
 
-# Functie om alle leds op een strip een bepaalde kleur te geven
+# Function to give every LED a color
 def color_wipe(strip, color, wait):
     for i in range(len(strip)):
         strip[i] = color
         strip.show()
         time.sleep(wait)
 
-# Voorbeeld: Alle leds op beide strips rood maken
-color_wipe(strip1, (255, 0, 0), 0.1)  # Rood
-color_wipe(strip2, (255, 0, 0), 0.1)  # Rood
+# Put all LED's on red
+color_wipe(strip1, (255, 0, 0), 0.1)  # Red
+color_wipe(strip2, (255, 0, 0), 0.1)  # Red
